89d545d74c643ab3563e1d6a2f3cbf9c8184ca74
Vincent Ivos <vincentivos1@gmail.com>
2020-09-15 23:36:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end  
+
+def using_poparg (array)
+  array.pop_with_arg(2)
+end  
+
+def using_shift(array, string)
+  array.shift(string)
+end  
+  
+def using_shift(array, string)
+  array.shift(2)
+end  
+  
